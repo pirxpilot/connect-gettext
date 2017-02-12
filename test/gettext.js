dd0c149ff4340fa1102927ee1dfc0da7584d2c6b
Damian Krzeminski <pirxpilot@code42day.com>
2017-02-11 17:08:43 -0800

    add support for message context (pgettext)
    when parsing .po file save multiple message dictionary, each in an
    object keyed by the dictionary context
    
    reimplement translate method to take 2 arguments - `msgctx` and `msgid`,
    standard `gettext` is bound to empty '' context
    see: https://www.gnu.org/software/gettext/manual/html_node/Contexts.html

--- test/gettext.js
+++ test/gettext.js
@@ -31,6 +31,28 @@ describe('connect-gettext node module', function () {
     });
   });
 
+  it('must support context for the default language', function (done) {
+    var locale = {
+      supportedLanguages: ['pl'],
+      defaultLanguage: 'en'
+    };
+    var connectGettext = require('..')(locale);
+    var req = {
+      lang: 'en'
+    };
+    var res = {
+      locals: {}
+    };
+
+    connectGettext(req, res, function(err) {
+      res.locals.should.have.property('gettext')
+        .with.type('function').property('name', 'identity');
+      var gettext = res.locals.gettext;
+      gettext.p('formal', 'Hello').should.be.exactly('Hello');
+      done(err);
+    });
+  });
+
   it('must inject default translation for missing language', function (done) {
     var locale = {
       supportedLanguages: ['pl'],
@@ -99,6 +121,27 @@ describe('connect-gettext node module', function () {
       gettext('Hello').should.be.exactly('Cześć');
       gettext('Good-bye').should.be.exactly('Do Widzenia');
       gettext('Some other string').should.be.exactly('Some other string');
+      done(err);
+    });
+  });
+
+  it('must honor message context', function (done) {
+    var locale = {
+      supportedLanguages: ['pl'],
+      defaultLanguage: 'en',
+    };
+    var connectGettext = require('..')(locale);
+    var req = {
+      lang: 'pl'
+    };
+    var res = {
+      locals: {}
+    };
+
+    connectGettext(req, res, function(err) {
+      var gettext = res.locals.gettext;
+      gettext('Hello').should.be.exactly('Cześć');
+      gettext.p('formal', 'Hello').should.be.exactly('Witamy');
       done(err);
     });
   });
